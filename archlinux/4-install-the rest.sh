207b8de20b33b799f7dc9ddb3947cb92c35881ac
William Chanrico <williamchanrico@gmail.com>
2018-09-02 19:21:57 +0700

    Add dconf-editor and gnome shell exts note

--- archlinux/4-install-the rest.sh
+++ archlinux/4-install-the rest.sh
@@ -3,7 +3,7 @@
 # Synchronize package databases
 sudo pacman -Sy
 
-sudo pacman -S --noconfirm arc-gtk-theme filemanager-actions uget imagemagick lshw libreoffice-fresh jre10-openjdk jdk10-openjdk openjdk10-doc deepin-screenshot gcolor2 tmux tcpdump htop iftop gimp mpv dosfstools tree bind-tools pavucontrol smartmontools traceroute xdotool ttf-dejavu ttf-liberation adobe-source-han-sans-otc-fonts ttf-hanazono go go-tools terminator zenity p7zip unrar rsync a52dec libmad x264 gst-libav gst-plugins-ugly dnscrypt-proxy totem
+sudo pacman -S --noconfirm arc-gtk-theme filemanager-actions uget imagemagick lshw libreoffice-fresh jre10-openjdk jdk10-openjdk openjdk10-doc deepin-screenshot gcolor2 tmux tcpdump htop iftop gimp mpv dosfstools tree bind-tools pavucontrol smartmontools traceroute xdotool ttf-dejavu ttf-liberation adobe-source-han-sans-otc-fonts ttf-hanazono go go-tools terminator zenity p7zip unrar rsync a52dec libmad x264 gst-libav gst-plugins-ugly dnscrypt-proxy totem dconf-editor
 
 sudo systemctl enable dnscrypt-proxy
 sudo systemctl start dnscrypt-proxy
@@ -21,14 +21,26 @@ rm -rf ~/.dropbox-dist
 install -dm0 ~/.dropbox-dist
 
 # Post-install messages
+echo "Notes:"
+echo ""
 echo "=== DNSCrypt ==="
 echo "Uncomment 'name_servers=127.0.0.1' in /etc/resolvconf.conf"
 echo "Select resolver for dnscrypt at '/etc/dnscrypt-proxy/dnscrypt-proxy.toml'"
 echo "server_names = ['cloudflare', 'cloudflare-ipv6']"
 echo "Don't forget to restart dnscrypt-proxy.service"
-echo "https://welcome.opendns.com/welcome/"
 echo ""
 echo "=== FileManager-Actions ==="
 echo "Config for 'Open in Terminal' with terminator"
 echo "Path: terminator"
 echo "Parameters: --working-directory=%f"
+echo ""
+echo "=== Gnome Exts ==="
+echo "Alternatetab"
+echo "Application Menu"
+echo "Dash to dock"
+echo "Places status indicator"
+echo "Removable drive menu"
+echo "Sound input & output device chooser"
+echo "Uptime indicator"
+echo "User themes"
+echo "System-monitor"
