5e7d79006aeabb64032a4bf8030ea8144efd22f1
William Chanrico <williamchanrico@gmail.com>
2018-09-03 00:02:46 +0700

    Improve and add more custom dconf config

---
+++ archlinux/5-custom-dconf.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Title: custom-dconf 
+# Description: Apply dconf config 
+# Author: William Chanrico
+# Date: 2-Sept-2018 
+
+ARRAY=( "dconf/gnome-extentions:/org/gnome/shell/extensions"
+        "dconf/gnome-desktop-keybindings:/org/gnome/desktop/wm"
+        "dconf/gnome-media-keybindings:/org/gnome/settings-daemon/plugins" )
+
+for data in "${ARRAY[@]}"; do
+	dir="${data%%:*}"
+	dconf_key="${data##*:}"
+
+	for filename_full in $dir/*.dconf; do
+		filepath=${filename_full%%.dconf}
+		filename=${filepath##*/}
+		echo "dconf load $dconf_key/$filename/ < $filename_full"
+		dconf load $dconf_key/$filename/ < $filename_full
+	done
+done
+
