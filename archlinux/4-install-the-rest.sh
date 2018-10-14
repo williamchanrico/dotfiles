3764ca1bf65e6322ca0006fe9429285a5539b5da
William Chanrico <williamchanrico@gmail.com>
2018-10-14 23:36:20 +0700

    Remove dnscrypt-proxy

--- archlinux/4-install-the-rest.sh
+++ archlinux/4-install-the-rest.sh
@@ -4,14 +4,14 @@
 sudo pacman -Sy
 
 sudo pacman -S --noconfirm \
-	arc-gtk-theme filemanager-actions uget imagemagick lshw libreoffice-fresh jre10-openjdk \
-	jdk10-openjdk openjdk10-doc deepin-screenshot gcolor2 tmux tcpdump htop iftop gimp mpv dosfstools tree bind-tools \
-	pavucontrol smartmontools traceroute xdotool ttf-dejavu ttf-liberation adobe-source-han-sans-otc-fonts ttf-hanazono \
-	go go-tools terminator zenity p7zip unrar rsync a52dec libmad x264 gst-libav gst-plugins-ugly dnscrypt-proxy totem \
-	dconf-editor ntfs-3g jq tcpdump asciinema
-
-sudo systemctl enable dnscrypt-proxy
-sudo systemctl start dnscrypt-proxy
+	arc-gtk-theme filemanager-actions uget imagemagick lshw \
+	libreoffice-fresh jre10-openjdk jdk10-openjdk openjdk10-doc \
+	deepin-screenshot gcolor2 tmux tcpdump htop iftop gimp mpv \
+	dosfstools tree bind-tools pavucontrol smartmontools traceroute \
+	xdotool ttf-dejavu ttf-liberation adobe-source-han-sans-otc-fonts \
+	ttf-hanazono go go-tools terminator zenity p7zip unrar rsync a52dec \
+	libmad x264 gst-libav gst-plugins-ugly totem dconf-editor ntfs-3g \
+	jq tcpdump asciinema
 
 # Setup golang
 mkdir -p ~/src/go/{src,bin}
@@ -20,8 +20,8 @@ mkdir -p ~/src/go/{src,bin}
 sudo mv -vi /usr/lib/nautilus/extensions-3.0/libterminal-nautilus.so{,.bak}
 
 yay -S --noconfirm \
-	dropbox nautilus-dropbox transmission-gtk peek adobe-source-han-sans-otc-fonts nvm spotify visual-studio-code-bin \
-	vokoscreen
+	dropbox nautilus-dropbox transmission-gtk peek vokoscreen \
+	adobe-source-han-sans-otc-fonts nvm spotify visual-studio-code-bin	
 
 # Prevent dropbox automatic updates
 rm -rf ~/.dropbox-dist
@@ -29,13 +29,6 @@ install -dm0 ~/.dropbox-dist
 
 # Post-install messages
 echo "Notes:"
-echo ""
-echo "=== DNSCrypt ==="
-echo "Uncomment 'name_servers=127.0.0.1' in /etc/resolvconf.conf"
-echo "Select resolver for dnscrypt at '/etc/dnscrypt-proxy/dnscrypt-proxy.toml'"
-echo "server_names = ['cloudflare', 'cloudflare-ipv6']"
-echo "Don't forget to restart dnscrypt-proxy.service"
-echo ""
 echo "=== Gnome Exts ==="
 echo "Alternatetab"
 echo "Application Menu"
