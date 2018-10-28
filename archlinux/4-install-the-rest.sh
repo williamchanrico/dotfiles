86d5cfacecf71c7354d7c0feea3bd2a72eecd151
William Chanrico <williamchanrico@gmail.com>
2018-10-28 16:23:06 +0700

    Add betterlockscreen-git package

--- archlinux/4-install-the-rest.sh
+++ archlinux/4-install-the-rest.sh
@@ -21,7 +21,8 @@ sudo mv -vi /usr/lib/nautilus/extensions-3.0/libterminal-nautilus.so{,.bak}
 
 yay -S --noconfirm \
 	dropbox nautilus-dropbox transmission-gtk peek vokoscreen \
-	adobe-source-han-sans-otc-fonts nvm spotify visual-studio-code-bin
+	adobe-source-han-sans-otc-fonts nvm spotify visual-studio-code-bin \
+	betterlockscreen-git
 
 # Prevent dropbox automatic updates
 rm -rf ~/.dropbox-dist
