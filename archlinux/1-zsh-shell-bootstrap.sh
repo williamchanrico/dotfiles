2d98af73dfcaeca64726cb59bfb248c82033210b
William Chanrico <williamchanrico@gmail.com>
2018-12-05 18:23:07 +0700

    Add vim and gvim

--- archlinux/1-zsh-shell-bootstrap.sh
+++ archlinux/1-zsh-shell-bootstrap.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 
 # Install zsh
-sudo pacman -S --noconfirm zsh wget curl git
+sudo pacman -S --noconfirm zsh wget curl git vim gvim
 chsh -s /usr/bin/zsh william
 
 # Install oh-my-zsh
