7742cb60ecbc0247cec2b99656eb1a8ff79d5476
William Chanrico <williamchanrico@gmail.com>
2019-02-05 17:54:18 +0700

    Add z package

--- i3/2-install-common.sh
+++ i3/2-install-common.sh
@@ -9,7 +9,7 @@ sudo pacman -S --needed --noconfirm \
 	gst-plugins-ugly totem dconf-editor jq asciinema expat restic figlet xsel \
 	clang cowsay fzf xcb-util-xrm tldr the_silver_searcher prettier lsof pngquant \
 	dmenu xcape xautolock gsimplecal lxappearance compton rofi zathura zathura-pdf-mupdf \
-	feh gpicview ranger mediainfo w3m highlight clipmenu clipnotify rxvt-unicode
+	feh gpicview ranger mediainfo w3m highlight clipmenu clipnotify rxvt-unicode z
 yay -S --needed --noconfirm \
 	peek vokoscreen nvm spotify-stable \
 	global ntfy networkmanager-dmenu-git polybar urxvt-perls-git
