8f8a5b3c3a2679e84eb0218e192bf0764a2eebac
William Chanrico <williamchanrico@gmail.com>
2019-05-17 08:59:46 +0700

    Disable beep noises

--- setup/0-i3-gnome-intel-desktop-bootstrap.sh
+++ setup/0-i3-gnome-intel-desktop-bootstrap.sh
@@ -10,5 +10,9 @@ sudo pacman -S --noconfirm xorg-server xorg-apps xf86-video-intel \
 	mesa gnome i3 networkmanager network-manager-applet nm-connection-editor
 yay -S --noconfirm i3-gnome
 
+# Disable beep noises
+sudo rmmod pcspkr
+echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
+
 sudo systemctl enable NetworkManager
 sudo systemctl enable gdm
