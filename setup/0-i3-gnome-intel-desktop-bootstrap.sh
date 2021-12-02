ca182e73f66dbf9db50a8e438c0e20de37a42770
William Chanrico <williamchanrico@gmail.com>
2021-12-02 21:41:28 +0700

    Reorder yay

--- setup/0-i3-gnome-intel-desktop-bootstrap.sh
+++ setup/0-i3-gnome-intel-desktop-bootstrap.sh
@@ -8,6 +8,25 @@ sudo pacman -Sy
 
 sudo pacman -S --noconfirm xorg-server xorg-apps xf86-video-intel \
 	mesa gnome i3 networkmanager network-manager-applet nm-connection-editor
+
+# Install yay
+if [ ! -x "$(command -v yay)" ]; then
+	# Install dependencies
+	sudo pacman -S --needed --noconfirm git
+
+	# Create a temp working dir and navigate into it
+	mkdir -p /tmp/yay_install
+	cd /tmp/yay_install || exit 1
+
+	# Install yay from git
+	git clone https://aur.archlinux.org/yay.git .
+	makepkg --noconfirm -si
+
+	# Clean up
+	cd ~ || exit 1
+	rm -rf /tmp/yay_install
+fi
+
 yay -S --noconfirm i3-gnome
 
 # Disable beep noises
--- setup/1-zsh-shell-bootstrap.sh
+++ setup/1-zsh-shell-bootstrap.sh
@@ -14,24 +14,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 	exit 1
 }
 
-# Install yay
-if [ ! -x "$(command -v yay)" ]; then
-	# Install dependencies
-	sudo pacman -S --needed --noconfirm git
-
-	# Create a temp working dir and navigate into it
-	mkdir -p /tmp/yay_install
-	cd /tmp/yay_install || exit 1
-
-	# Install yay from git
-	git clone https://aur.archlinux.org/yay.git .
-	makepkg --noconfirm -si
-
-	# Clean up
-	cd ~ || exit 1
-	rm -rf /tmp/yay_install
-fi
-
 # Install google-chrome and gnome-shell-integration
 yay -S --needed --noconfirm google-chrome chrome-gnome-shell
 
