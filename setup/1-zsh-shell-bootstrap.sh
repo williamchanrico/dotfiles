26302a0b6ba70626d8b3b8d2efde1af7a8beb872
William Chanrico <williamchanrico@gmail.com>
2022-01-29 01:53:54 +0700

    Add comment regarding BitBucket App Password

--- setup/1-zsh-shell-bootstrap.sh
+++ setup/1-zsh-shell-bootstrap.sh
@@ -19,8 +19,9 @@ yay -S --needed --noconfirm google-chrome chrome-gnome-shell
 
 # Set up YADM and dotfiles
 yay -S --needed --noconfirm yadm-git
+echo "Generate App Password: https://support.atlassian.com/bitbucket-cloud/docs/app-passwords/"
 yadm clone https://bitbucket.org/williamchanrico/dotfiles
-#yadm decrypt
+# yadm decrypt
 
 # Install powerline fonts
 sudo pacman -S --needed --noconfirm awesome-terminal-fonts
