f356efcf20f2699926175c49f49c9a0e2eaf0cee
William Chanrico <williamchanrico@gmail.com>
2022-12-04 15:49:45 +0700

    Refactor

--- setup/2-install-common.sh
+++ setup/2-install-common.sh
@@ -2,40 +2,199 @@
 
 # Install common packages
 sudo pacman -S --needed --noconfirm \
-	filemanager-actions imagemagick lshw libreoffice-fresh flameshot \
-	gcolor2 tmux gimp mpv xdotool dosfstools tree pavucontrol smartmontools \
-	zenity p7zip unrar rsync a52dec ntfs-3g libmad x264 gst-libav gstreamer-vaapi \
-	gst-plugins-ugly totem dconf-editor jq asciinema expat restic figlet xsel \
-	clang cowsay fzf xcb-util-xrm tldr the_silver_searcher prettier lsof pngquant \
-	dmenu xcape xautolock gsimplecal lxappearance compton rofi zathura zathura-pdf-mupdf \
-	feh ranger mediainfo w3m highlight clipmenu clipnotify rxvt-unicode z dunst zsh-completions \
-	gettext shellcheck blueman pulseaudio-alsa pulseaudio-bluetooth bluez-utils redshift \
-	arch-audit xxd hexyl rsync
+	# File manager
+	ranger \
+	# GNOME-based File Manager extension to allow launching programs via context menu
+	filemanager-actions \
+	# Image compression/manipulation
+	imagemagick \
+	lshw \
+	libreoffice-fresh \
+	# Screenshot
+	flameshot \
+	# Color picker
+	gcolor2 \
+	tmux \
+	gimp \
+	mpv \
+	# X automation
+	xdotool \
+	# Filesystem helper
+	dosfstools \
+	tree \
+	# Disk health
+	smartmontools \
+	# GNOME port of dialog windows
+	zenity \
+	p7zip \
+	unrar \
+	# GNOME config editor
+	dconf-editor \
+	# Parser for JSON
+	jq \
+	# Parser for YAML
+	yq \
+	asciinema \
+	# Parser for XML
+	expat \
+	# Backup tool
+	restic \
+	# ASCII art generator
+	figlet \
+	# Clipboard selection and manipulation
+	xsel \
+	# C++ compiler API
+	clang \
+	# ASCII art generator
+	cowsay \
+	# Fuzzy finder
+	fzf \
+	# XCB utility functions for the X resource manager
+	xcb-util-xrm \
+	# Man pages but TLDR version
+	tldr \
+	# Prettifier
+	prettier \
+	lsof \
+	# PNG compression
+	pngquant \
+	# Launcher
+	dmenu \
+	# Dmenu alternative
+	rofi \
+	# Configure modifier keys to act as other keys
+	xcape \
+	# Monitor X activity and can trigger a lock program
+	xautolock \
+	# Calendar
+	gsimplecal \
+	# GTK+ theme switcher of the LXDE
+	lxappearance \
+	# Compositor for X
+	compton \
+	# PDF reader
+	zathura \
+	# Support mupd rendering (MuPDF library can parse and render PDF, XPS, and EPUB)
+	zathura-pdf-mupdf \
+	# Image viewer
+	feh \
+	# Text-based browser
+	w3m \
+	# Converts sourcecode to HTML, XHTML, RTF, LaTeX, TeX, SVG, BBCode and terminal escape sequences with coloured syntax highlighting
+	highlight \
+	# Clipboard manager
+	clipmenu \
+	clipnotify \
+	# Terminal
+	rxvt-unicode \
+	# Directory jumper
+	z \
+	# Notification daemon
+	dunst \
+	# Shell auto completions
+	zsh-completions \
+	# Internationalization and localization system
+	gettext \
+	# Linter for shell scripts
+	shellcheck \
+	# Show audio/video files
+	mediainfo \
+	# Bluetooth manager
+	blueman \
+	bluez-utils \
+	# Audio manager
+	pulseaudio-alsa \
+	pulseaudio-bluetooth \
+	# Audio control
+	pavucontrol \
+	# Decoder for ATSC A/52 streams
+	a52dec \
+	# Decoder for MPEG audio
+	libmad \
+	# Decoder for H.264/MPEG-4 AVC
+	x264 \
+	# Multimedia toolkit libraries
+	gst-libav \
+	gstreamer-vaapi \
+	gst-plugins-ugly \
+	# Only need the totem-video-thumbnailer
+	totem \
+	# Display blue light dimmer
+	redshift \
+	# Similar to pkg-audit based on Arch Security Team data
+	arch-audit \
+	# Hex dump/viewer
+	xxd \
+	hexyl \
+	# Backblaze cloud storage CLI
+	backblaze-b2 \
+	# Parser for binary
+	fq \
+	# Monitoring nvme disk
+	nvme-cli \
+	# Monitoring system
+	htop \
+	# Vulkan toolkit
+	vulkan-tools
 yay -S --needed --noconfirm \
-	nvm spotify nordic-theme-git papirus-folders-nordic \
-	python-pulsectl python-rofi \
-	global ntfy networkmanager-dmenu-git urxvt-perls-git yq i3-volume \
+	# NodeJS manager
+	nvm \
+	spotify \
+	nordic-theme-git \
+	papirus-folders-nordic \
+	python-pulsectl \
+	python-rofi \
+	global \
+	# Notification tool
+	ntfy \
+	networkmanager-dmenu-git \
+	# Rxvt terminal Perl extension
+	urxvt-perls-git \
+	i3-volume \
 	sublime-text-dev
-
-# Fonts
-sudo pacman -S --needed --noconfirm \
-	ttf-dejavu ttf-font-awesome noto-fonts noto-fonts-cjk noto-fonts-emoji \
-	ttf-hanazono adobe-source-han-sans-otc-fonts
-yay -S --needed --noconfirm ttf-awesome-fonts nerd-fonts-dejavu-complete \
-	ttf-nerd-fonts-symbols ttf-font-icons ttf-ms-fonts
 
 # Network related tools
 sudo pacman -S --needed --noconfirm \
-	netcat tcpdump htop iftop bind-tools traceroute tcpdump nmap mtr \
-	aria2 whois vpnc yt-dlp
+	netcat \
+	tcpdump \
+	iftop \
+	nethogs \
+	bind-tools \
+	traceroute \
+	tcpdump \
+	nmap \
+	mtr \
+	aria2 \
+	whois \
+	vpnc \
+	# Youtube-dl alternative
+	yt-dlp \
+	rsync
 yay -S --needed --noconfirm \
-	transmission-cli transmission-remote-gtk transmission-gtk sshrc
-
+	transmission-cli \
+	transmission-remote-gtk \
+	transmission-gtk
 # Too lazy to type sudo & password
 sudo setcap cap_net_raw=eip "$(command -v iftop)"
 
 # Setup python
-sudo pacman -S --needed --noconfirm python python2 python-pip pyenv python-pipenv \
-	python-virtualenv python-pylint \
-	python-black flake8 python-pynvim \
-	python-psutil python-netifaces
+sudo pacman -S --needed --noconfirm python python-pip pyenv python-pipenv \
+	python-virtualenv \
+	python-pylint \
+	python-black \
+	flake8 \
+	python-pynvim \
+	python-psutil \
+	python-netifaces
+
+# Fonts
+sudo pacman -S --needed --noconfirm \
+	ttf-dejavu \
+	ttf-font-awesome \
+	noto-fonts \
+	noto-fonts-cjk \
+	noto-fonts-emoji \
+	ttf-hanazono \
+	adobe-source-han-sans-otc-fonts
+yay -S --needed --noconfirm ttf-awesome-fonts nerd-fonts-dejavu-complete \
+	ttf-nerd-fonts-symbols ttf-font-icons ttf-ms-fonts
