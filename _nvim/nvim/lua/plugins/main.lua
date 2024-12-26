044c86e6a20c6d0171bf7b950d2d807c6a7d7a35
William Chanrico <williamchanrico@gmail.com>
2024-12-26 16:10:17 +0700

    Replace vim-airline with lualine

--- _nvim/nvim/lua/plugins/main.lua
+++ _nvim/nvim/lua/plugins/main.lua
@@ -612,17 +612,66 @@ return {
 		end,
 	},
 
-	-- Airline
+	-- Statusline
 	{
-		"vim-airline/vim-airline",
-		dependencies = {
-			"vim-airline/vim-airline-themes",
-		},
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
-			vim.g.airline_theme = "minimalist"
-			vim.g.airline_powerline_fonts = 0
+			require('lualine').setup {
+				options = {
+					icons_enabled = true,
+					theme = 'onedark',
+					-- component_separators = { left = '', right = '' },
+					-- section_separators = { left = '', right = '' },
+					section_separators = '',
+					component_separators = '',
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					always_show_tabline = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 100,
+						tabline = 100,
+						winbar = 100,
+					}
+				},
+				sections = {
+					lualine_a = { 'mode' },
+					lualine_b = { 'branch', 'diff', 'diagnostics' },
+					lualine_c = { 'filename' },
+					lualine_x = { 'encoding', 'fileformat', 'filetype' },
+					lualine_y = { 'progress' },
+					lualine_z = { 'location' }
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { 'filename' },
+					lualine_x = { 'location' },
+					lualine_y = {},
+					lualine_z = {}
+				},
+				tabline = {},
+				winbar = {},
+				inactive_winbar = {},
+				extensions = {}
+			}
 		end,
 	},
+	-- {
+	-- 	"vim-airline/vim-airline",
+	-- 	dependencies = {
+	-- 		"vim-airline/vim-airline-themes",
+	-- 	},
+	-- 	config = function()
+	-- 		vim.g.airline_theme = "minimalist"
+	-- 		vim.g.airline_powerline_fonts = 1
+	-- 	end,
+	-- },
 
 	-- Telescope
 	{
